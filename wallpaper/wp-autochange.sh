7d52dd05b0926fa91175a8d4611baa00a251c803
恼人转转愁生 <lianguangran@gmail.com>
2021-01-03 03:03:54 +0800

    first commit

---
+++ wallpaper/wp-autochange.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true;
+do
+	/bin/bash ~/.config/i3/wallpaper/wp-change.sh
+	sleep 1m
+done
---
+++ wallpaper/wp-change.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# feh --recursive --randomize --bg-fill $HOME/Pictures/wallpapers
+nitrogen --random --set-zoom-fill $HOME/Picture/wallpaper
